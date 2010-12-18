68030de481f279b423a176afc450ae220d128954
Tim Horton <hortont424@gmail.com>
2010-12-18 17:17:27 -0500

    Add JSON framework and a JSON version of our theme (no loader yet)

---
+++ Themes/tango.js
@@ -0,0 +1,35 @@
+{
+    "name": "Tango",
+    "author": "Tim Horton",
+    "fonts": {
+        "normal": {"name": "Menlo", "size": 12},
+        "bold": {"name": "Menlo Bold", "size": 12},
+        "italic": {"name": "Menlo Italic", "size": 12}
+    },
+    "colors": {
+        "normal": {r: 0.0, g: 0.0, b: 0.0, a: 1.0},
+
+        "background.source": {r: 0.0, g: 0.0, b: 0.0, a: 1.0},
+        "background.output": {r: 0.0, g: 0.0, b: 0.0, a: 1.0},
+        "background.comment": {r: 0.0, g: 0.0, b: 0.0, a: 1.0},
+
+        "highlight.comment": {r: 0.5, g: 0.5, b: 0.5, a: 1.0},
+        "highlight.keyword": {r: 0.306, g: 0.604, b: 0.024, a: 1.0},
+        "highlight.number": {r: 0.125, g: 0.290, b: 0.529, a: 1.0},
+        "highlight.string": {r: 0.804, g: 0.361, b: 0.0, a: 1.0},
+
+        "status.default": {r: 0.729, g: 0.741, b: 0.714, a: 1.0},
+        "status.busy": {r: 0.988, g: 0.914, b: 0.310, a: 1.0},
+        "status.failure": {r: 0.788, g: 0.0, b: 0.0, a: 1.0},
+        "status.success": {r: 0.451, g: 0.824, b: 0.086, a: 1.0},
+    },
+    "highlight":
+    {
+        "comment": {"color": "highlight.comment", "font": "italic"},
+        "keyword": {"color": "highlight.keyword", "font": "bold"},
+        "number": {"color": "highlight.number", "font": "normal"},
+        "string": {"color": "highlight.string", "font": "normal"}
+    },
+    "cellAnimationSpeed": 0.2,
+    "cellSpacing": 4.0
+}
