289ca8084a08053b9ea82e05b6e3b878cf1694ae
Tim Horton <hortont424@gmail.com>
2011-02-02 16:42:11 -0500

    Add the sample file that I use on the website

---
+++ Samples/simple/simple.py
@@ -0,0 +1,20 @@
+print "Hello, world!"
+
+#--
+
+# Simple Fibonacci sequence generator
+
+a, b = 1, 1
+
+while a < 100:
+	print a
+	a, b = b, a + b
+
+#--
+
+print )abc
+
+#--
+
+import random
+print random.uniform(0, 42)
