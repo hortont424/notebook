6f00c9ed02ac676a4f352715a4ce5ab70bb3eda0
Tim Horton <hortont424@gmail.com>
2011-05-08 16:57:59 -0400

    missed some scons output highlighting

--- SConscripts/PrettyPrint.py
+++ SConscripts/PrettyPrint.py
@@ -26,10 +26,12 @@ def TOOL_PRETTY_PRINT(env):
         ARCOMSTR = message("Linking static library", False),
         SHLINKCOMSTR = message("Linking shared library", False),
         LINKCOMSTR = message("Linking program", False),
+        LDMODULECOMSTR = message("Linking module", False),
         INSTALLSTR = message("Copying"),
         IBTOOLCOMSTR = message("Compiling XIB"),
         WRITEVALCOMSTR = message("Writing value to file", False),
         LATEXCOMSTR = message("Rendering LaTeX file"),
-        INLINECLKERNELCOMSTR = message("Inlining CL kernels", False))
+        INLINECLKERNELCOMSTR = message("Inlining CL kernels", False),
+        RANLIBCOMSTR = message("Generating archive index", False))
 
     return env
